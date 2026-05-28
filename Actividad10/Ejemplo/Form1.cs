e2bc59e66c588c8e2232ee9688594c8f2a87495a
unknown <valentin.munoz03@gmail.com>
2026-05-28 09:06:11 -0300

    Ejemplo

---
+++ Actividad10/Ejemplo/Form1.cs
@@ -0,0 +1,15 @@
+namespace Ejemplo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Text = this.textBox1.Text;
+        }
+    }
+}
